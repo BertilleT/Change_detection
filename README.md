de82963c0473efa2a6335694fed6ac8658829228
Bertille Temple <bertilletemple@yahoo.fr>
2024-03-27 11:22:16 +0100

    clean files and update readme

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
 Project on Change Detection from AI and Environment course, MVA 2024. 
 
+<<<<<<< Updated upstream
 The script for the OSCD datset were taken from : https://github.com/rcdaudt/fully_convolutional_change_detection.   
 
 Data:
   - OSCD: https://ieee-dataport.org/open-access/oscd-onera-satellite-change-detection.   
   - HRSCD: https://ieee-dataport.org/open-access/hrscd-high-resolution-semantic-change-detection-dataset
+=======
+Data: https://ieee-dataport.org/open-access/oscd-onera-satellite-change-detection. 
+
+
+## Src folder description
+
+- `binary_CD_HRSCD.ipynb`: Jupyter notebook for performing binary change detection on the High Resolution Semantic Change Detection (HRSCD) dataset. 
+
+- `binary_CD_OSCD.ipynb`: Jupyter notebook for binary change detection on the OSCD (Other Satellite Change Detection) dataset. Code from https://github.com/rcdaudt/fully_convolutional_change_detection. 
+
+- `siamunet_conc.py`: Implementation of the Siamese U-Net with a concatenation approach, for binary change detection.
+
+- `siamunet_diff.py`: Implementation of the Siamese U-Net with a difference approach, for binary change detection. 
+
+- `split_HRSCD_train_valid_test.py`: Script for splitting the HRSCD dataset into training, validation, and test sets, ensuring a balanced distribution.
+
+- `unet.py`: The U-Net architecture, a foundational model for semantic segmentation tasks in satellite image analysis.
+>>>>>>> Stashed changes
--- src/split_HRSCD_train_valid_test.py
+++ src/split_HRSCD_train_valid_test.py
@@ -8,41 +8,58 @@ import random
 import numpy as np
 import pandas as pd
 
-def split_dataset(img_path, trg_path):
+def split_dataset(img_path_2006, img_path_2012, trg_path):
     # load the RGB images form the dataset
-    images = os.listdir(img_path)
+    images = os.listdir(img_path_2012)
     images = [x.split('.')[0] for x in images]
-    print(images)
+    images_2006 = os.listdir(img_path_2006)
     images = np.array(images)
     np.random.shuffle(images)
     # split the dataset
     train = images[:int(0.6*len(images))]
+    train_codes = [x.split('-')[2] + '-' + x.split('-')[3] for x in train]
     valid = images[int(0.6*len(images)):int(0.8*len(images))]
+    valid_codes = [x.split('-')[2] + '-' + x.split('-')[3] for x in valid]
     test = images[int(0.8*len(images)):]
+    test_codes = [x.split('-')[2] + '-' + x.split('-')[3] for x in test]
     # save the train_images in train_foler
-    train_folder_images = '../data/HRSCD/train/images'
-    train_folder_targets = '../data/HRSCD/train/targets'
-    for image in train:
-        os.rename(img_path + '/' + image + '.tif', train_folder_images + '/' + image + '.tif')
-        os.rename(trg_path + '/' + image + '.tif', train_folder_targets + '/' + image + '.tif')
+    train_2006_images = '../data/HRSCD/train/images/2006'
+    train_2012_images = '../data/HRSCD/train/images/2012'
+    train_targets = '../data/HRSCD/train/targets'
+    for i, image in enumerate(train):
+        # check if if train_codes[i] is a substring of one of the elments from images_2006 and store the string containing it
+        name_img_2006 = [x for x in images_2006 if train_codes[i] in x]
+        os.rename(img_path_2006 + '/' + name_img_2006[0], train_2006_images + '/' + train_codes[i] + '.tif')
+        # check if train_codes
+        os.rename(img_path_2012 + '/' + image + '.tif', train_2012_images + '/' + train_codes[i] + '.tif')
+        os.rename(trg_path + '/' + image + '.tif', train_targets + '/' + train_codes[i] + '.tif')
     # save the valid_images in valid_foler
-    valid_folder_images = '../data/HRSCD/valid/images'
-    valid_folder_targets = '../data/HRSCD/valid/targets'
-    for image in valid:
-        os.rename(img_path + '/' + image + '.tif', valid_folder_images + '/' + image + '.tif')
-        os.rename(trg_path + '/' + image + '.tif', valid_folder_targets + '/' + image + '.tif')
+    valid_2006_images = '../data/HRSCD/valid/images/2006'
+    valid_2012_images = '../data/HRSCD/valid/images/2012'
+    valid_targets = '../data/HRSCD/valid/targets'
+
+    for i, image in enumerate(valid):
+        name_img_2006 = [x for x in images_2006 if valid_codes[i] in x]
+        os.rename(img_path_2006 + '/' + name_img_2006[0], valid_2006_images + '/' + valid_codes[i] + '.tif')
+        os.rename(img_path_2012 + '/' + image + '.tif', valid_2012_images + '/' + valid_codes[i] + '.tif')
+        os.rename(trg_path + '/' + image + '.tif', valid_targets + '/' + valid_codes[i] + '.tif')
     # save the test_images in test_foler
-    test_folder_images = '../data/HRSCD/test/images'
-    test_folder_targets = '../data/HRSCD/test/targets'
-    for image in test:
-        os.rename(img_path + '/' + image + '.tif', test_folder_images + '/' + image + '.tif')
-        os.rename(trg_path + '/' + image + '.tif', test_folder_targets + '/' + image + '.tif')
+    test_2006_images = '../data/HRSCD/test/images/2006'
+    test_2012_images = '../data/HRSCD/test/images/2012'
+    test_targets = '../data/HRSCD/test/targets'
+
+    for i, image in enumerate(test):
+        name_img_2006 = [x for x in images_2006 if test_codes[i] in x]
+        os.rename(img_path_2006 + '/' + name_img_2006[0], test_2006_images + '/' + test_codes[i] + '.tif')
+        os.rename(img_path_2012 + '/' + image + '.tif', test_2012_images + '/' + test_codes[i] + '.tif')
+        os.rename(trg_path + '/' + image + '.tif', test_targets + '/' + test_codes[i] + '.tif')
     
     print('Dataset splitted in train, valid and test')
         
 
 folder_path = '../data/HRSCD'
-images_path = folder_path + '/images'
+images_path_2006 = folder_path + '/2006'
+images_path_2012 = folder_path + '/2012'
 targets_path = folder_path + '/targets'
 
-split_dataset(images_path, targets_path)
+split_dataset(images_path_2006, images_path_2012, targets_path)
